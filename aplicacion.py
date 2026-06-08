592c10d9b9e05d530c70a55c09b98fdc3f0a9cfa
LotoMetrikard079 <lotometrikard@gmail.com>
2026-06-08 15:51:24 -0400

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -1,2 +1,18 @@
+
 import streamlit as st
-st.title("LOTOMETRIKA RD")
+
+st.set_page_config(
+    page_title="LOTOMETRIKA RD",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("📊 LOTOMETRIKA RD")
+
+st.subheader("Motor Estadístico y Predictivo")
+
+st.write(
+    "Plataforma de análisis de loterías de República Dominicana."
+)
+
+st.success("Sistema funcionando correctamente.")
